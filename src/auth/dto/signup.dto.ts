e6cf04826decfe32d65478f06aaf457ab9aa93a6
jeeke <jeeke7@gmail.com>
2020-11-11 20:20:00 +0530

    swagger-docs

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,32 +1,34 @@
-import {
-    IsEmail,
-    IsNotEmpty,
-    IsString,
-    MinLength
-} from "class-validator";
+import {IsEmail, IsNotEmpty, IsString, MinLength} from "class-validator";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class SignupDto {
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     phone: string;
 
+    @ApiProperty()
     @IsEmail()
     email: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     @MinLength(6)
     password: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     country: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     city: string;
